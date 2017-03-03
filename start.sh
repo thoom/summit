15cc3fdc5ca4e28f315d0af5af23e5993225f6bb
Z.d.Peacock <zdp@thoomtech.com>
2017-03-03 14:00:34 -0700

    Updated start script to only update the ssh stuff when pulling from git.

--- start.sh
+++ start.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Disable Strict Host checking for non interactive git clones
-mkdir -p -m 0700 /root/.ssh
-echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
-
-if [ ! -z "$SSH_KEY" ]; then
-  echo $SSH_KEY > /root/.ssh/id_rsa.base64
-  base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
-  chmod 600 /root/.ssh/id_rsa
-fi
-
 cd /app
 
 # Dont pull code down if the .git folder exists
 if [ ! -d ".git" ]; then
+
+ # Disable Strict Host checking for non interactive git clones
+ mkdir -p -m 0700 /root/.ssh
+ echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
+
+ if [ ! -z "$SSH_KEY" ]; then
+  echo $SSH_KEY > /root/.ssh/id_rsa.base64
+  base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
+  chmod 600 /root/.ssh/id_rsa
+ fi
+
   # Pull down code from git for our site!
   if [ ! -z "$GIT_REPO" ]; then
     # Remove the test index file
@@ -24,15 +25,16 @@ if [ ! -d ".git" ]; then
       git clone $GIT_REPO /app
     fi
   fi
+
+  if [ -f "Gemfile" ]; then
+   bundle install --without=development
+  fi
 fi
 
+# Always check for a config.ru
 if [ ! -f "config.ru" ]; then
   echo "config.ru missing"
   exit 1
-fi
-
-if [ -f "Gemfile" ]; then
-  bundle install --without=development
 fi
 
 # Start thin
